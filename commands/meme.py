677d9da1807a3479c7f3fde7751dc3ad4d96f8fd
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-07-07 17:40:35 +0100

    add order option for meme command

--- commands/meme.py
+++ commands/meme.py
@@ -4,11 +4,12 @@ import requests
 import praw 
 import re
 import os
-    
+import random 
+
 class meme:
     def __init__(self):
         self.help_message = "meme - Returns a meme from Reddit."
-        self.usage_message = "Incorrect usage. Try follow : meme [-s subreddit name] [-t time frame {h,d,w,m,a}]"
+        self.usage_message = "Try follow : meme [-s subreddit name] [-t time frame {h,d,w,m,a}] [-o order {top,new,hot}]"
         self.name = "meme"
         
         config = configparser.ConfigParser()
@@ -25,15 +26,19 @@ class meme:
         self.sub = "memes"
         self.time = "h"
         self.amount = 1
-        self.images_path = 1
+        self.order = None
+        self.images_path = f'images/{self.sub}/'
         
     def parse_args(self,string):
         print("got ",string)
+        if string=="":
+            return self.random()
         parser = ArgumentParser.ArgumentParser()
-        req_args = parser.add_argument_group("required arguments")
-        req_args.add_argument('-s',"--subreddit",type=str,help="The subreddit you want to download the image from",required=True)
-        req_args.add_argument('-a',"--amount",type=int,help="The amount of images you want to download",required=False,default=1)
-        req_args.add_argument('-t',"--time",type=str,help="Time frame  ",choices=["h","d","w","m","y"], required=True)
+        opt_args = parser.add_argument_group("Optional arguments")
+        opt_args.add_argument('-s',"--subreddit",type=str,help="The subreddit you want to download the image from",required=False,default="memes")
+        opt_args.add_argument('-a',"--amount",type=int,help="The amount of images you want to download",required=False,default=1)
+        opt_args.add_argument('-t',"--time",type=str,help="Time frame",choices=["h","d","w","m","y"], required=False,default="h")
+        opt_args.add_argument('-o',"--order",type=str,help="Order",choices=["new","top","hot"], required=False)
         
         try:
             args = parser.parse_args(string.split())
@@ -44,8 +49,15 @@ class meme:
         self.sub = args.subreddit
         self.amount = args.amount
         self.time = args.time
+        self.order = args.order
         self.images_path = f'images/{self.sub}/'
-        return self.start()
+        
+        if self.order==None or self.order=="top":
+            return self.top()
+        elif self.order=="new":
+            return self.new()
+        elif self.order=="hot":
+            return self.hot()
     
     def help_message(self):
         return self.help_message
@@ -60,12 +72,20 @@ class meme:
         with open(image['fname'],'wb') as f:
             f.write(r.content)
 
-    def start(self):
+    def get_link(self,submission):
+        if not submission.stickied and submission.url.endswith(('jpg', 'jpeg', 'png')):
+            fname = self.images_path + re.search('(?s:.*)\w/(.*)', submission.url).group(1)
+            return {"url":submission.url,"fname":fname}
+                
+        
+    def get_image(self,order):
         images = []
+        orders = {"top":self.reddit.subreddit(self.sub).top(time_filter=self.timeframe[self.time]),
+                  "new":self.reddit.subreddit(self.sub).new(),
+                  "hot":self.reddit.subreddit(self.sub).hot()}
         try:
             count = 0
-            submissions = self.reddit.subreddit(self.sub).top(time_filter=self.timeframe[self.time])
-            
+            submissions = orders[order]
             for submission in submissions:
                 if not submission.stickied and submission.url.endswith(('jpg', 'jpeg', 'png')):
                     fname = self.images_path + re.search('(?s:.*)\w/(.*)', submission.url).group(1)
@@ -76,6 +96,7 @@ class meme:
                             break
                     else:
                         return ("image",fname)
+                    
             if len(images):
                 if not os.path.exists(self.images_path):
                     os.makedirs(self.images_path)
@@ -85,3 +106,22 @@ class meme:
         except Exception as e:
             print(e)
             return ("error",e)
+        
+    def random(self):
+        submissions = self.reddit.subreddit(self.sub).top(time_filter=self.timeframe[self.time])
+        image_links = list(map(self.get_link,submissions))
+        random_int = random.randint(0,len(image_links))
+        image = image_links[random_int]
+        if not os.path.exists(self.images_path):
+            os.makedirs(self.images_path)
+        self.download_image(image)
+        return ("image",image["fname"])
+    
+    def top(self):
+        return self.get_image("top")    
+    
+    def new(self):
+        return self.get_image("new")
+
+    def hot(self):
+        return self.get_image("hot")
