10ed37319ae366477731c3dac284c7a0968c4604
JaydipMagan <25053933+JaydipMagan@users.noreply.github.com>
2020-07-16 18:51:51 +0100

    integrated mememaker with bot

--- commands/mememaker.py
+++ commands/mememaker.py
@@ -1,11 +1,3 @@
-class mememaker:
-
-    def __init__(self):
-        self.help_message = "mememaker - Creates a meme using the text provided"
-        self.usage_message = "Try follow : mememaker [template] [text]"
-        self.name = "mememaker"
-    def parse_args(self,string):
-        pass
-    
-    def help_message(self):
-        return self.help_message
+import commands.mm.meme_maker as mm 
+class mememaker(mm.mememaker):
+    pass
